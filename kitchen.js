758a08adbf0b89c2e8b9b4317f104d55fd8d421f
Sanvi9 <74234358+Sanvi9@users.noreply.github.com>
2021-11-21 16:18:03 +0530

    Add files via upload

---
+++ kitchen.js
@@ -0,0 +1,12 @@
+img = "";
+function preload() {
+    img = loadImage("kitchen.jpeg");
+}
+
+function setup() {
+    canvas = createCanvas(600,450);
+    canvas.center();
+}
+function draw() {
+    image(img,0,0,600,450);
+}
---
+++ main.js
@@ -0,0 +1,12 @@
+
+function bedroom() {
+    window.location = "bedroom.html"
+}
+
+function living_room() {
+    window.location = "livingRoom.html"
+}
+
+function kitchen() {
+    window.location = "kitchen.html"
+}
